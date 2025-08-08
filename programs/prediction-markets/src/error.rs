a0185ec07ff1fab82ef4ab81c455a26dc1f74407
laksayvaishnav <thelakshayvaishnav@gmail.com>
2025-08-09 03:44:02 +0530

    added error codes

--- programs/prediction-markets/src/error.rs
+++ programs/prediction-markets/src/error.rs
@@ -1,7 +1,17 @@
 use anchor_lang::prelude::*;
 
 #[error_code]
-pub enum ErrorCode {
-    #[msg("Custom error message")]
-    CustomError,
+pub enum BetError {
+    #[msg("Bet start time must be before end time")]
+    InvalidTime,
+    #[msg("Betting window closed or not started")]
+    OutsideBettingWindow,
+    #[msg("Bet not yet resolved")]
+    NotResolved,
+    #[msg("Outcome mismatch for this side")]
+    WrongOutcome,
+    #[msg("Cannot resolve before end time unless creator")]
+    CannotResolveYet,
+    #[msg("Bonding curve calculation error")]
+    CurveError,
 }
