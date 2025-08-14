8a0dcd12b0651d4c3624e7717013cc54033dc650
laksayvaishnav <thelakshayvaishnav@gmail.com>
2025-08-15 03:52:02 +0530

    added ctx for buy shares

--- programs/prediction-markets/src/instructions/buy_shares.rs
+++ programs/prediction-markets/src/instructions/buy_shares.rs
@@ -1,7 +1,9 @@
 use anchor_lang::prelude::*;
-use anchor_spl::{ associated_token::AssociatedToken, token_interface::TokenInterface };
+use anchor_spl::{ associated_token::AssociatedToken, token_interface::{Mint, TokenAccount, TokenInterface} };
 
-use crate::{ bettor, platform_config, Bettor, MarketError, Wager, BETTOR_PROFILE };
+use crate::{
+    bettor, platform_config, Bettor, Market, MarketError, PlatformConfig, Wager, BETTOR_PROFILE, BETTOR_WALLET, MARKET, MARKET_VAULT, MINT_NO, MINT_YES, PLATFORM_CONFIG, WAGER
+};
 
 #[derive(Accounts)]
 pub struct BuyShares<'info> {
@@ -16,12 +18,77 @@ pub struct BuyShares<'info> {
     )]
     pub bettor_profile: Account<'info, Bettor>,
 
+    
+
+    #[account(
+        mut,
+        seeds = [PLATFORM_CONFIG],
+        bump = platform_config.config_bump
+    )]
+    pub platform_config: Account<'info, PlatformConfig>,
+
+    #[account(
+        mut,
+        seeds = [MARKET, platform_config.key().to_bytes().as_ref(), &market.market_name.as_bytes()[..32]],
+        bump = market.market_bump
+    )]
+    pub market: Account<'info, Market>,
+
     #[account(
         init_if_needed,
         payer = bettor,
-        space = Wager::DISCRIMINATOR.len() + Wager::INIT_SPACE
+        space = Wager::DISCRIMINATOR.len() + Wager::INIT_SPACE,
+        seeds = [WAGER, market.key().to_bytes().as_ref(), bettor.key().to_bytes().as_ref()],
+        bump
     )]
     pub wager_account: Account<'info, Wager>,
+
+    #[account(
+        mut,
+        seeds = [BETTOR_WALLET,bettor.key().to_bytes().as_ref(), platform_config.key().to_bytes().as_ref()],
+        bump = bettor_profile.bettor_vault_bump
+    )]
+    pub bettor_wallet_account: SystemAccount<'info>,
+
+    
+    #[account(
+        mut,
+        seeds = [MINT_YES,market.key().to_bytes().as_ref()],
+        bump = market.mint_yes_bump,
+        mint::authority = platform_config,
+    )]
+    pub mint_yes: Box<InterfaceAccount<'info, Mint>>,
+
+    #[account(
+        mut,
+        seeds = [MINT_NO,market.key().to_bytes().as_ref()],
+        bump = market.mint_no_bump,
+        mint::authority = platform_config,
+    )]
+    pub mint_no: Box<InterfaceAccount<'info, Mint>>,
+
+    #[account(
+        mut,
+        seeds = [MARKET_VAULT,market.key().to_bytes().as_ref()],
+        bump  = market.market_vault_bump
+    )]
+    pub market_vault_account: SystemAccount<'info>,
+
+    #[account(
+        init_if_needed,
+        payer = bettor,
+        associated_token::authority = bettor,
+        associated_token::mint = mint_yes
+    )]
+    pub bettor_yes_account: Box<InterfaceAccount<'info, TokenAccount>>,
+
+    #[account(
+        init_if_needed,
+        payer = bettor,
+        associated_token::authority = bettor,
+        associated_token::mint = mint_no
+    )]
+    pub bettor_no_account: Box<InterfaceAccount<'info, TokenAccount>>,
 
     pub system_program: Program<'info, System>,
     pub token_program: Interface<'info, TokenInterface>,
