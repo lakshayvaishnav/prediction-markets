20f7cc5883426cc64f4cea3e97f9df7144552787
laksayvaishnav <thelakshayvaishnav@gmail.com>
2025-08-15 17:00:17 +0530

    added send shares and deposit wager helper functions

--- programs/prediction-markets/src/instructions/buy_shares.rs
+++ programs/prediction-markets/src/instructions/buy_shares.rs
@@ -1,9 +1,36 @@
-use anchor_lang::prelude::*;
-use anchor_spl::{ associated_token::AssociatedToken, token_interface::{Mint, TokenAccount, TokenInterface} };
+use anchor_lang::{
+    prelude::*,
+    solana_program::native_token::LAMPORTS_PER_SOL,
+    system_program::{ transfer, Transfer },
+};
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_2022::MintToChecked,
+    token_interface::{ mint_to_checked, Mint, TokenAccount, TokenInterface },
+};
 
 use crate::{
-    bettor, platform_config, Bettor, Market, MarketError, PlatformConfig, Wager, BETTOR_PROFILE, BETTOR_WALLET, MARKET, MARKET_VAULT, MINT_NO, MINT_YES, PLATFORM_CONFIG, WAGER
+    bettor,
+    check_ban,
+    check_zero,
+    decimal_convo,
+    platform_config,
+    Bettor,
+    Market,
+    MarketError,
+    PlatformConfig,
+    Wager,
+    BETTOR_PROFILE,
+    BETTOR_WALLET,
+    MARKET,
+    MARKET_VAULT,
+    MINT_NO,
+    MINT_YES,
+    PLATFORM_CONFIG,
+    WAGER,
 };
+
+use rust_decimal::prelude::*;
 
 #[derive(Accounts)]
 pub struct BuyShares<'info> {
@@ -17,8 +44,6 @@ pub struct BuyShares<'info> {
         constraint = bettor_profile.bettor_pubkey == bettor.key() @ MarketError::InvalidAccount
     )]
     pub bettor_profile: Account<'info, Bettor>,
-
-    
 
     #[account(
         mut,
@@ -50,7 +75,6 @@ pub struct BuyShares<'info> {
     )]
     pub bettor_wallet_account: SystemAccount<'info>,
 
-    
     #[account(
         mut,
         seeds = [MINT_YES,market.key().to_bytes().as_ref()],
@@ -93,4 +117,118 @@ pub struct BuyShares<'info> {
     pub system_program: Program<'info, System>,
     pub token_program: Interface<'info, TokenInterface>,
     pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+impl<'info> BuyShares<'info> {
+    pub fn buy_shares(
+        &mut self,
+        bumps: BuySharesBumps,
+        shares_amount: u64,
+        is_yes: bool
+    ) -> Result<()> {
+        check_ban!(self.bettor_profile.is_ban);
+
+        match self.market.market_state {
+            crate::MarketStatus::Active => {
+                // save data
+                if !self.wager_account.is_initialized {
+                    self.wager_account.set_inner(Wager {
+                        market_pubkey: self.market.key(),
+                        bet_amount_spent: 0,
+                        bet_amount_earned: 0,
+                        market_status: self.market.market_state,
+                        market_outcome: crate::MarketOutcome::NotResolved,
+                        yes_shares: 0,
+                        no_shares: 0,
+                        is_initialized: true,
+                        bet_bump: bumps.wager_account,
+                    });
+                }
+
+                check_zero!([decimal_convo!(shares_amount)]);
+
+                // calculate the cost of given shares
+                let share_cost = match is_yes {
+                    ture =>
+                        self.market
+                            .share_calculation(true, shares_amount, 0, self.platform_config.fees)?
+                            .to_u64()
+                            .ok_or(MarketError::ArithemeticError)?,
+
+                    false =>
+                        self.market
+                            .share_calculation(true, 0, shares_amount, self.platform_config.fees)?
+                            .to_u64()
+                            .ok_or(MarketError::ArithemeticError)?,
+                };
+
+                // left todo
+            }
+        }
+        todo!()
+    }
+
+    fn deposit_wager(&mut self, amount: u64) -> Result<()> {
+        // transfer wager amount from bettor wallet to vault accounts.
+
+        require!(
+            self.bettor_wallet_account.lamports() >= amount * LAMPORTS_PER_SOL,
+            MarketError::NotEnoughAmount
+        );
+
+        let accounts = Transfer {
+            from: self.bettor_wallet_account.to_account_info(),
+            to: self.market_vault_account.to_account_info(),
+        };
+
+        let bettor_seeds = self.bettor.key().to_bytes();
+        let platform_config_seeds = self.platform_config.key().to_bytes();
+
+        let seeds = &[
+            BETTOR_WALLET,
+            bettor_seeds.as_ref(),
+            platform_config_seeds.as_ref(),
+            &[self.bettor_profile.bettor_vault_bump],
+        ];
+
+        let signer_seeds = &[&seeds[..]];
+
+        let ctx = CpiContext::new_with_signer(
+            self.token_program.to_account_info(),
+            accounts,
+            signer_seeds
+        );
+
+        transfer(ctx, amount * LAMPORTS_PER_SOL)?;
+
+        Ok(())
+    }
+
+    fn send_shares(&mut self, share_amount: u64, is_yes: bool) -> Result<()> {
+        // transfering shares to bettor token accounts
+
+        let (to, mint) = match is_yes {
+            true => (&self.bettor_yes_account, &self.mint_yes),
+            false => (&self.bettor_no_account, &self.mint_no),
+        };
+
+        let accounts = MintToChecked {
+            to: to.to_account_info(),
+            mint: mint.to_account_info(),
+            authority: self.platform_config.to_account_info(),
+        };
+
+        let seeds = &[PLATFORM_CONFIG, &[self.platform_config.config_bump]];
+        let signer_seeds = &[&seeds[..]];
+
+        let ctx = CpiContext::new_with_signer(
+            self.token_program.to_account_info(),
+            accounts,
+            signer_seeds
+        );
+
+        mint_to_checked(ctx, share_amount, mint.decimals)?;
+
+        Ok(())
+    }
 }
